597397e44894a20746569281cb6e1e8afd36d9f4
Pavel Afanasev <afansvme@yandex.ru>
2020-02-25 22:31:51 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/afansv/gorm-update-mask
+
+go 1.13
+
+require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/fatih/structtag v1.2.0 // indirect
+	github.com/stoewer/go-strcase v1.1.0 // indirect
+)
---
+++ mask.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"reflect"
+	"strings"
+
+	"github.com/fatih/structs"
+	"github.com/stoewer/go-strcase"
+)
+
+func Mask(model interface{}, updatePaths []string) (map[string]interface{}, error) {
+	t := reflect.TypeOf(model)
+
+	fields := structs.Names(model)
+
+	mask := make(map[string]interface{})
+
+	for _, fName := range fields {
+		field, _ := t.FieldByName(fName)
+
+		columnName, ok := parseColumnName(field)
+		if !ok {
+			columnName = strcase.SnakeCase(fName)
+		}
+
+		if isStringInArray(columnName, updatePaths) {
+			mask[columnName] = reflect.ValueOf(model).FieldByName(fName).Interface()
+		}
+	}
+
+	return mask, nil
+}
+
+func isStringInArray(s string, ar []string) bool {
+	for _, a := range ar {
+		if a == s {
+			return true
+		}
+	}
+
+	return false
+}
+
+func parseColumnName(field reflect.StructField) (columnName string, ok bool) {
+	tag, ok := field.Tag.Lookup("gorm")
+	if !ok {
+		tag, ok = field.Tag.Lookup("sql")
+		if !ok {
+			return "", false
+		}
+	}
+
+	structValues := strings.Split(tag, ";")
+	for _, v := range structValues {
+		kv := strings.Split(v, ":")
+		if len(kv) != 2 {
+			continue
+		}
+
+		if kv[0] == "column" {
+			return kv[1], true
+		}
+	}
+
+	return "", false
+}
